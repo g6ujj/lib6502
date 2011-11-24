d4ce78f0a23d288cef49ac1bb2af18a4665a4a88
Neil Stoker <gg6ujj@gmail.com>
2011-11-24 16:57:55 +0000

    LGPL Licence added to library.
    Added text for LGPL to project and added the boilerplate text to each of the
    files.

--- readIntelHex.h
+++ readIntelHex.h
@@ -1,3 +1,25 @@
+/*
+ readIntelHex.h
+
+ Author:
+      Neil Stoker <gg6ujj@gmail.com>
+
+ Copyright (c) 2011 Neil Stoker
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef READINTELHEX_H
 #define READINTELHEX_H
 
@@ -35,7 +57,7 @@ class readIntelHex
         bool openFile(string fName,Memory *ram);
     protected:
     private:
-    //string errMessage;
+    //errMessage;
 
 
     errHRec loadLine(string theLine,Memory *ram);
